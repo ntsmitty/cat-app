b903a517b2b76e2b886fb29fbef9739f12445412
Nick <nicktsmith7@gmail.com>
2019-05-08 16:32:34 -0700

    feat: add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+// Requirements
+
+//    *  Use React to build your application
+
+//    *  Generate an API authentication token and manage it within the application.
+
+//    *  Build a UI that will display the data digested from the API.
+
+//    *  Should include a view that displays images within a scrollable table view.
+
+//    *  Should allow the user to favorite an image(i.e. jpeg/png, and gifs).
+
+//    *  Should provide a section or area or page that lists the user's favorites.
+
+//    *  Provide a README about this application and how-to-run-it.
+
+ 
+
+// Bonus (but not required)
+
+//   *  Navigation for browsing by category.
+
+//   *  Allow for filtering of images by jpeg, png, gif, and category (default to all).
+
+//   *  Provide an alert or notification if/when API is offline.
+
+//   *  Utilize tests when needed (e.g. unit, cases, mocks).
+
+ 
+
+// Notes
+
+//   *   Do not worry about design. A simple UI boilerplate or native UI elements will do.
+
+//   *   Completeness is not required, do enough of each requirement to show your understanding of the requirement and your approach.
