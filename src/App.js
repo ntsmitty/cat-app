44226dbf3405771067b81a561a0cb7e48d637fc3
Nick <nicktsmith7@gmail.com>
2019-05-08 18:19:48 -0700

    feat: make request to cat api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-  
-  );
+const API_KEY = "4569480f-b88e-4125-a1dd-1404ce45929e";
+const CAT_URI = "https://api.thecatapi.com/v1/images/search/?limit=10";
+
+export class App extends Component {
+  componentDidMount() {
+    fetch(CAT_URI, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(json => console.log(JSON.stringify(json)))
+      .catch(err => console.log(err));
+  }
+  render() {
+    return <div />;
+  }
 }
 
 export default App;
